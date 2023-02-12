01241ea7e296c81771ad570a0ac4e22f54d7ab8c
Vitaly <poroshenkoe@gmail.com>
2023-02-12 13:01:47 +0300

    add: script to send item name to popup

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector('.popup');
+const popupName = document.querySelector('.popup__name');
 const body = document.querySelector('body');
 const arr = document.querySelectorAll('.card__button');
 const popupBtn = document.querySelector('.popup__button');
@@ -6,13 +7,15 @@ const back = document.querySelector('.background');
 
 arr.forEach((el, id) => {
   el.addEventListener('click', () => {
-    show();
+    show(el.value);
   });
 });
 
-function show() {
+function show(text) {
   popup.classList.add('active');
   body.classList.add('active');
+  popupName.innerHTML = `Покупка товара: ${text}`;
+
   window.scrollTo(0, 0);
 
   document.addEventListener('click', (e) => {
